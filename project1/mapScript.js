bcc561392c2ed71520bb8e13b900571c93def340
jayesh <jayeshred20@gmail.com>
2025-07-23 15:02:12 +0100

    add updates

--- project1/mapScript.js
+++ project1/mapScript.js
@@ -1,13 +1,188 @@
 
-var map =null;
+let map = null;
+let baseLayers = {};
+let layerControl = null;
+let countryBorderLayer = null;
+let capitalMarker = null;
 
-//initializing map
-map = L.map('map').setView([0, 0], 1.5);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+// Loader functions
+function showLoader() {
+    document.getElementById("loader").style.display = "block";
+}
 
+function hideLoader() {
+    document.getElementById("loader").style.display = "none";
+}
+showLoader();
+
+//function to return base Map layers
+function getBaseLayers() {
+    return {
+        'Open Street Map': L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }),
+        'Airports': L.tileLayer('https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: 'Map <a href="https://memomaps.de/">memomaps.de</a>, © OpenStreetMap contributors'
+        }),
+        'Satellite': L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg', {
+            maxZoom: 20,
+            attribution: '&copy; CNES, Airbus DS, PlanetObserver, OpenMapTiles, OpenStreetMap'
+        }),
+        'Watercolor': L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg', {
+            maxZoom: 16,
+            attribution: '&copy; Stamen, Stadia Maps, OpenMapTiles, OpenStreetMap'
+        }),
+    };
+}
+
+//function to return weather data
+function printweather(lat, lng){
+    showLoader();
+    $.ajax({
+        url: "weather.php",
+        type: "GET",
+        dataType: "json",
+        data: { 
+            lat: lat, 
+            lng: lng, 
+        },
+        success: function(result) {
+            const data = result.data.weatherObservation;
+
+            document.getElementById("wDateTime").innerHTML = data.datetime;
+            document.getElementById("station").innerHTML = data.stationName;
+            document.getElementById("temp").innerHTML = `${data.temperature} deg Cel`;
+            document.getElementById("clouds").innerHTML = data.clouds;
+            document.getElementById("humidity").innerHTML = data.humidity;
+            document.getElementById("dewPoint").innerHTML = data.dewPoint;
+            document.getElementById("windSpeed").innerHTML = `${data.windSpeed} knots`;
+            
+            hideLoader();
+        },
+        error: function(err) {
+            console.error("Details AJAX error", err);
+            hideLoader();
+        }
+    });
+}
+
+//function to return time data
+function printTimeInfo(lat, lng){
+    showLoader();
+    $.ajax({
+        url: "timezone.php",
+        type: "GET",
+        dataType: "json",
+        data: { 
+            lat: lat, 
+            lng: lng, 
+        },
+        success: function(result) {
+            const data = result.data;
+
+            document.getElementById("timezoneId").innerHTML = data.timezoneId;
+            document.getElementById("sunrise").innerHTML = data.sunrise.split(" ")[1];
+            document.getElementById("sunset").innerHTML = data.sunset.split(" ")[1];
+            document.getElementById("cTime").innerHTML = data.time.split(" ")[1];
+            document.getElementById("cDate").innerHTML = data.time.split(" ")[0];
+            
+            hideLoader();
+        },
+        error: function(err) {
+            console.error("Details AJAX error", err);
+            hideLoader();
+        }
+    });
+}
+
+//function to load country details
+function loadCountryDetails(countryCode) {
+    showLoader();
+    $.ajax({
+        url: "countryDetails.php",
+        type: "GET",
+        dataType: "json",
+        data: { code: countryCode },
+        success: function(result) {
+            const data = result.data;
+
+            const langHTML = `<p>${Object.values(data.languages).join(", ")}</p>`;
+            const flagHTML = `<img src="${data.flags.svg}" alt="${data.flags.alt}" width="60"></p>`;
+
+            let currencyHTML = "";
+            for (const [code, value] of Object.entries(data.currencies)) {
+                currencyHTML += `<p>${value.name} (${value.symbol}) [${code}]</p>`;
+            }
+            document.getElementById("cName").innerHTML = data.name.common;
+            document.getElementById("capital").innerHTML = data.capital[0];
+            document.getElementById("capital2").innerHTML = data.capital[0];
+            document.getElementById("continent").innerHTML = data.continents[0];
+            document.getElementById("currency").innerHTML = currencyHTML;
+            document.getElementById("language").innerHTML = langHTML;
+            document.getElementById("population").innerHTML = data.population.toLocaleString();
+            document.getElementById("area").innerHTML = data.area.toLocaleString()+ " km²";
+            document.getElementById("gMap").innerHTML = `<a href="${data.maps.googleMaps}" target="_blank">View in Google Maps</a>`;
+            document.getElementById("flag").innerHTML = flagHTML;
+            document.getElementById("tZone").innerHTML = data.timezones;
+
+
+            const latlng = data.capitalInfo.latlng;
+            capitalMarker = L.marker(latlng).addTo(map).bindPopup(`Capital: <strong>${data.capital[0]}</strong>`).openPopup();
+            printweather(latlng[0],latlng[1]);
+            printTimeInfo(latlng[0],latlng[1]);
+            hideLoader();
+        },
+        error: function(err) {
+            console.error("Details AJAX error", err);
+            hideLoader();
+        }
+    });
+}
+
+//function to load country dropdown
+function loadCountryDropdown() {
+    showLoader();
+    $.ajax({
+        url: 'load.php',
+        type: 'GET',
+        dataType: 'json',
+        success: function(result) {
+            let options = '<option value="">Select a Country</option>';
+            let countries = result.data.filter(c => c.cioc).sort((a, b) => a.name.common.localeCompare(b.name.common));
+
+            for (let country of countries) {
+                options += `<option value="${country.cioc}">${country.name.common}</option>`;
+            }
+
+            $('#country').html(options);
+            hideLoader();
+        },
+        error: function(err) {
+            console.error("Dropdown AJAX error", err);
+            hideLoader();
+        }
+    });
+}
+
+//initializing map layers on page load
+$(document).ready(() => {
+    baseLayers = getBaseLayers();
+
+    map = L.map('map', {
+        center: [0, 0],
+        zoom: 2,
+        layers: [baseLayers['Open Street Map']]
+    });
+
+    layerControl = L.control.layers(baseLayers).addTo(map);
+
+    loadCountryDropdown();
+    hideLoader();
+});
+
+//border highlight style
 function borderHighlightStyle(){
     return{
         weight: 2,
@@ -18,206 +193,129 @@ function borderHighlightStyle(){
     }
 }
 
-function createMap(lat, lng, zoom=6){
-
-    if (map !== null) {
-        map.remove();
-        map = null;
+//function to create map
+function createMap(lat, lng, zoom = 6) {
+    showLoader();
+    // Remove old country border
+    if (countryBorderLayer) {
+        map.removeLayer(countryBorderLayer);
     }
 
-    map = L.map('map').setView([lat, lng], zoom);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+    if (capitalMarker) {
+        map.removeLayer(capitalMarker);
+    }
 
-    //adding current country border with ajax and php
+    // Get and show current country border
     $.ajax({
-        url:"countryBorders.php",
+        url: "countryBorders.php",
         type: "GET",
         dataType: "json",
-        success: async function(data){
-            console.log(data.data);
-            let matched = false;
+        success: function(data) {
             const point = [lng, lat];
-            for (let feature of data.features){
-                let geometry = feature.geometry;
+            let matchedFeature = null;
 
-                if(geometry.type == "Polygon"){
-                    matched = turf.booleanPointInPolygon(point, turf.polygon(geometry.coordinates));
-                
-                } else if(geometry.type == "MultiPolygon"){
-                    for(coords of geometry.coordinates){
-                        if(turf.booleanPointInPolygon(point, turf.polygon(coords))){
-                            matched = true;
-                            
+            for (let feature of data.features) {
+                const geometry = feature.geometry;
+
+                if (geometry.type === "Polygon") {
+                    if (turf.booleanPointInPolygon(point, turf.polygon(geometry.coordinates))) {
+                        matchedFeature = feature;
+                        break;
+                    }
+                } else if (geometry.type === "MultiPolygon") {
+                    for (const coords of geometry.coordinates) {
+                        if (turf.booleanPointInPolygon(point, turf.polygon(coords))) {
+                            matchedFeature = feature;
+                            break;
                         }
                     }
                 }
-                
-                if(matched){
-                    console.log("result from feature");
-                    console.log(feature);
-                    //adding and higlighting the current country border
-                    let layer = L.geoJson(feature, {style: borderHighlightStyle}).addTo(map);
-                    map.fitBounds(layer.getBounds(), {padding: [5,5]});                      
-                    let countryCode= feature.properties.iso_a3;
-                    console.log(countryCode);
-
-
-                    //request to get country details
-                    $.ajax({
-                        url: "countryDetails.php",
-                        type:'GET',
-                        dataType:'json',
-                        data:{
-                            code: countryCode
-                        },
-                        success: function(result){
-                            console.log("inside coutry details req in main func");
-                            
-                            console.log(result);
-                            document.getElementById("overlay").style.visibility = "visible"; 
-                            let countryName= result.data.name.common;
-                            let capital= result.data.capital[0];
-                            let continent= result.data.continents[0];
-                            let population= result.data.population;
-                            let area= result.data.area;
-                            let currencies= result.data.currencies;
-
-                            let languages = result.data.languages;
-                            let langHTML = `<p>${Object.values(languages).join(", ")}</p>`;
-
-                            let flag = result.data.flags;
-                            let flagHTML = `<p><strong>Flag:</strong> <img src="${flag.svg}" alt="${flag.alt}" width="60"></p>`;
-
-                            let currencyHTML = "";
-                            for (const [key, value] of Object.entries(currencies)) {
-                                let currencyCode = key;
-                                let currencyName = value.name;
-                                let currencySymbol = value.symbol;
-                                currencyHTML += `<p> ${currencyName} (${currencySymbol}) [${currencyCode}]</p>`;
-                            }
-                            
-                            let gMapLink= result.data.maps.googleMaps;
-
-
-                            document.getElementById('overlay').innerHTML = `
-                                <h3>Country Info</h3>
-                                <p><strong>Name:</strong> ${countryName}</p>
-                                <p><strong>Capital:</strong> ${capital}</p>
-                                <p><strong>Continent:</strong> ${continent}</p>
-                                <p><strong>Population:</strong> ${population.toLocaleString()}</p>
-                                <p><strong>Area:</strong> ${area.toLocaleString()} km²</p>
-                                <p><strong>Currencies:</strong>${currencyHTML}</p>                                   
-                                <p><strong>Languages:</strong>${langHTML}</p>
-                                ${flagHTML}
-                                <a href= "${gMapLink}" target="_blank">View in Google Map.</a>
-                            `;
-
-                            let latlng = result.data.capitalInfo.latlng;
-                            var capitalMarker = L.marker(latlng).addTo(map);
-                            capitalMarker.bindPopup(`<p>Capital:<strong> ${capital}</strong></p>`).openPopup();
-                            
-
-                        },
-                        error: function(xyz, textStatus, error){
-                            console.log("AJAX error:", textStatus, error);
-                            console.log("Response:", xyz.responseText);
-                        }
-                    });
-
-                    }
-                }                                            
-            },
-            error: function(xyz, textStatus, error){
-                console.log("AJAX error:", textStatus, error);
-                console.log("Response:", xyz.responseText);
             }
-    });    
 
-    return map;
-}
+            if (matchedFeature) {
+                countryBorderLayer = L.geoJson(matchedFeature, { style: borderHighlightStyle() }).addTo(map);
+                map.fitBounds(countryBorderLayer.getBounds(), { padding: [0,0] });
 
-//function to load current country map. 
-$("#getLocation").on("click",()=>{
-    if(!navigator.geolocation){
-    alert("Geolocation is not supported by your browser!");
-    alert("Please select a country from the dropdown");
-}else{
-    navigator.geolocation.getCurrentPosition((position)=>{
-        let currLat= position.coords.latitude;
-        let currLng= position.coords.longitude;
-        createMap(currLat, currLng);
-    },
-    (error) => {
-        alert("Unable to retrieve your location. Please select a country.");
-        console.error("Geolocation error:", error);
+                const countryCode = matchedFeature.properties.iso_a3;
+                loadCountryDetails(countryCode);
+            }
+            hideLoader();
+        },
+        error: function(err) {
+            console.error("Borders AJAX error", err);
+            hideLoader();
+        }
     });
 }
-});
 
-//function to load countries in dropdown.
-$(document).ready(()=>{
+//function to load dropdown 
+function loadCountryDropdown() {
+    showLoader();
     $.ajax({
         url: 'load.php',
         type: 'GET',
         dataType: 'json',
         success: function(result) {
-
             let options = '<option value="">Select a Country</option>';
-            let countryArray = [];
+            let countries = result.data.filter(c => c.cioc).sort((a, b) => a.name.common.localeCompare(b.name.common));
 
-            for (let i in result.data) {
-                let cName = result.data[i];
-                countryArray.push(cName);
+            for (let country of countries) {
+                options += `<option value="${country.cioc}">${country.name.common}</option>`;
             }
-            countryArray.sort((a, b) => a.name.common.localeCompare(b.name.common));
-            
-            for (let j = 0; j < countryArray.length; j++) {
-                if(countryArray[j].cioc!=""){
-                    options += `<option value="${countryArray[j].cioc}">${countryArray[j].name.common}</option>`;
-                }else{
-                    continue;
-                }
-            }
+
             $('#country').html(options);
+            hideLoader();
         },
-        error: function(xhr, status, error) {
-            console.error('AJAX Error:', status, error);
+        error: function(err) {
+            console.error("Dropdown AJAX error", err);
+            hideLoader();
         }
     });
-})
+}
 
-//Event handler to update country.
-$('#countrySubmit').on('click',() => {
-    let countryCode = $("#country").val();
-    console.log(countryCode);
-    if(countryCode ==""){
-        alert("Please select a country");
-    }else{
-        //call navigator.geolocation with selected countryCode
-        $.ajax({
-            url: "countryCoords.php",
-            type:'GET',
-            dataType:'json',
-            data:{
-                code: countryCode,
-            },
-            success: function(result){
-                console.log("results from country coords, when submited");
-                console.log(result);
-                let lat = result.data.capitalInfo.latlng[0];
-                let lng = result.data.capitalInfo.latlng[1];
-                console.log(lat);
-                console.log(lng);
-                //calling create map with selected country coordinates
-                createMap(lat, lng);
-            },
-            error: function(xyz, textStatus, error){
-                console.log("AJAX error:", textStatus, error);
-            }                   
-        })
+//function to get current country map
+$('#getLocation').on('click', () => {
+    showLoader();
+    if (!navigator.geolocation) {
+        alert("Geolocation not supported. Please select a country.");
+        return;
     }
-    
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            createMap(position.coords.latitude, position.coords.longitude);
+            hideLoader();
+        },
+        (error) => {
+            alert("Could not get your location.");
+            console.error("Geolocation error:", error);
+            hideLoader();
+        }
+    );
+});
+
+// Dropdown change
+$('#country').on('change', () => {
+    showLoader();
+    const code = $('#country').val();
+    if (!code) {
+        alert("Please select a country.");
+        return;
+    }
+
+    $.ajax({
+        url: "countryCoords.php",
+        type: "GET",
+        dataType: "json",
+        data: { code },
+        success: function(result) {
+            const [lat, lng] = result.data.capitalInfo.latlng;
+            createMap(lat, lng);
+            hideLoader();
+        },
+        error: function(err) {
+            console.error("Coords AJAX error", err);
+            hideLoader();
+        }
+    });
 });
---
+++ project1/weather.php
@@ -0,0 +1,34 @@
+
+<?php
+$executionStartTime= microtime(true);
+
+$url = 'http://api.geonames.org/findNearByWeatherJSON?lat=' . urlencode($_GET["lat"]) . '&lng=' . urlencode($_GET["lng"]) . '&username=jayeshannam';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+$decode = json_decode($result,true);
+
+if (!isset($decode) || empty($decode)) {
+    $output["status"]["code"] = "400";
+    $output["status"]["name"] = "error";
+    $output["status"]["description"] = "No Country data found!";
+    $output["status"]["url"] = $url;
+    $output["data"] = null;
+} else {
+    $output["status"]["code"] = "200";
+    $output["status"]["name"] = "ok";
+    $output["status"]["description"] = "success";
+    $output["status"]["returnedIn"]= intval((microtime(true) - $executionStartTime) *1000). "ms";
+    $output["data"] = $decode;
+}
+
+header("Content-Type: application/json; charset=UTF-8");
+echo json_encode($output);
+
+?>
